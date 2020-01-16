4e211304d8740247ab8fa5c517e01788eed4699e
louxinghong <louxinghong@xinyunlian.com>
2020-01-16 10:09:57 +0800

    improvement: 初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  globals: {
+    saveAs: true
   },
+  extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
+  rules: {},
   parserOptions: {
     parser: '@typescript-eslint/parser'
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <el-button type="success" @click="onTouchMe">戳我</el-button>
+    <p>{{ count }}</p>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+interface labelValue {
+  label: string
+}
+
+@Component
+export default class Home extends Vue {
+  // data
+  // private _this: any = this
+  private count: number = 100
+  private list: string[] = []
+  private output = this.identity<string>('lgglgglggl')
+  private outputTwo = this.identity<number>(123)
+
+  /**
+   * methods
+   */
+  public async onTouchMe (count: number): Promise<void> {
+    await this.$message.success('12')
+    this.$notify.success({ title: '成功', message: '成功导出' })
+    await this.$message.success(`${this.pubMethod(1, 2)}`)
+    await this.$message.success(`${this.getNum(2)}`)
+    await this.$message.success(`${this.optionalName('lgg', 'ice bear')}`)
+    await this.$message.success(`${this.defaultName(undefined, 'ice bear')}`)
+    await this.$message.success(
+      `${this.listName('grizz', 'panda', 'iceBear', 'lgg')}`
+    )
+    await this.$message.success(`${this.printLabel({ label: 'size is 10' })}`)
+    alert(this.output)
+    alert(this.outputTwo)
+  }
+
+  public pubMethod: (one: number, two: number) => number = function (
+    x: number,
+    y: number
+  ): number {
+    return x + y
+  }
+
+  public getNum (x: number): number {
+    return x * x
+  }
+  public optionalName (firstName: string, lastName?: string) {
+    if (lastName) {
+      return firstName + '' + lastName
+    } else {
+      return firstName
+    }
+  }
+  public defaultName (firstName = 'Lgg', lastName: string) {
+    return firstName + '' + lastName
+  }
+  public listName (firstName: string, ...restName: string[]) {
+    return firstName + ' ' + restName.join(' ')
+  }
+
+  public printLabel (labelObj: labelValue): void {
+    console.log(labelObj.label)
+  }
+
+  public identity<T> (arg: T): T {
+    console.log('输入：' + arg)
+    return arg
   }
 }
 </script>
