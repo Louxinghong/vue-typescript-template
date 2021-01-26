f6335c0d7307fecc181ae0189b789e93a6d93684
lxh <1056868828@qq.com>
2021-01-26 09:49:20 +0800

    improvement：axios封装优化

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -2,18 +2,28 @@ import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
 
 axios.defaults.baseURL = 'https://api.bilibili.com'
 axios.defaults.timeout = 3000
-axios.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8'
+// axios.defaults.headers.post['Content-Type'] = 'application/json; charset=UTF-8'
 
 // post请求转换接口参数格式
 axios.defaults.transformRequest = [
   (data, config) => {
-    if (config['Content-Type'] === 'application/x-www-form-urlencoded') {
+    // 文件格式
+    if (
+      config['Content-Type'].toLowerCase() ===
+      'multipart/form-data;charset=utf-8'
+    ) {
+      return data
+    }
+    // FormData格式
+    else if (config['Content-Type'] === 'application/x-www-form-urlencoded') {
       const tempData: string[] = []
       Object.keys(data).forEach((item) => {
         tempData.push(`${item}=${data[item]}`)
       })
       return tempData.join('&')
-    } else {
+    }
+    // JSON格式
+    else {
       return JSON.stringify(data)
     }
   }
